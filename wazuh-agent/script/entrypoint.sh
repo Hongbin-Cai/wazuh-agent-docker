eb5ede66b20886e2711a72ca47d232e76ee5fa48
hongbin <hongbin.cai@workland.com>
2019-12-17 16:10:30 -0500

    wazuh-agent/docker/registeration,docker-listener
        1. add registeration process, so now Agent immediately connects with Manager once deployed.
            (check script/entrypoint.sh)
        2. add necessary installation commands for docker-listener module

--- wazuh-agent/script/entrypoint.sh
+++ wazuh-agent/script/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+# Register agent if ENV does not work # ref: Documentation > User manual > Registering agents Using the simple registration service 
+/var/ossec/bin/agent-auth -m wazuh-manager-master-0.wazuh-cluster.wazuh.svc.cluster.local && \
+service wazuh-agent start && \
+
 for script in `ls /entrypoint-scripts/*.sh | sort -n`; do
   bash "$script"
-
 done
 
 # Start Wazuh Server.
